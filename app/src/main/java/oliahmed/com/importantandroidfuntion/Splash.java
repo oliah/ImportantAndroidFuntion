64d9d3dea89383bc6e8a52f346794721ca3db41a
Oli Ahmed <oliahmed205@gmail.com>
2018-01-05 01:10:14 +0600

    1. Added Working Splash Screen 2. Internet Check at Splash Screen

---
+++ app/src/main/java/oliahmed/com/importantandroidfuntion/Splash.java
@@ -0,0 +1,75 @@
+package oliahmed.com.importantandroidfuntion;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.github.javiersantos.bottomdialogs.BottomDialog;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import oliahmed.com.importantandroidfuntion.Util.NetworkCheck;
+
+public class Splash extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startProcess();
+
+    }
+
+    private void startProcess() {
+        if (!NetworkCheck.isConnect(this)) {
+            dialogNoInternet();
+        } else {
+            startActivityMainDelaymain();
+        }
+    }
+
+    private void startActivityMainDelaymain() {
+        // Show splash screen for 2 seconds
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                Intent i = new Intent(Splash.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        };
+        new Timer().schedule(task, 3000);
+    }
+
+    public void dialogNoInternet() {
+
+        new BottomDialog.Builder(Splash.this)
+                .setTitle("NO INTERNET !")
+                .setContent("Please Reconnect With Working Internet")
+                .setPositiveText("OK")
+                .setPositiveBackgroundColorResource(R.color.colorPrimary)
+                //.setPositiveBackgroundColor(ContextCompat.getColor(Splash.this, R.color.colorPrimary)
+                .setPositiveTextColorResource(android.R.color.white)
+                //.setPositiveTextColor(ContextCompat.getColor(Splash.this, R.color.colorAccent)
+                .onPositive(new BottomDialog.ButtonCallback() {
+                    @Override
+                    public void onClick(BottomDialog dialog) {
+                        finish();
+                    }
+                }).show();
+    }
+
+}
