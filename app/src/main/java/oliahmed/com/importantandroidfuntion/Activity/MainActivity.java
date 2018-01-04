5bbba27c1a97136c4cd66af89344ade16d198fe6
Oli Ahmed <oliahmed205@gmail.com>
2018-01-05 01:39:52 +0600

    1. Added Working Splash Screen 2. Internet Check at Splash Screen 3. Main Activity UI Button 4. Background 5. Icon 6. Theme

--- app/src/main/java/oliahmed/com/importantandroidfuntion/Activity/MainActivity.java
+++ app/src/main/java/oliahmed/com/importantandroidfuntion/Activity/MainActivity.java
@@ -2,6 +2,7 @@ package oliahmed.com.importantandroidfuntion.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import oliahmed.com.importantandroidfuntion.R;
 
@@ -11,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_top);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
 }
